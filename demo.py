390bd3feaa64892047780d35dd8221062656d9da
Dave Shepherd <dave.shepherd@endor.me.uk>
2014-07-06 21:48:06 +0100

    General improvements

--- demo.py
+++ demo.py
@@ -4,23 +4,28 @@ from time import sleep
 bb = BlinkyTape('/dev/ttyACM0') #least on Mac OS X, this is the port to use!
 
 SHORT_WAIT = 0.05
-LONG_WAIT = 0.5
 COLOUR_OFF = [0,0,0]
 COLOUR_RED = [254,0,0]
 COLOUR_GREEN = [0,254,0]
 COLOUR_BLUE = [0,0,254]
-COLOUR_ORANGE = [254,140,0]
+COLOUR_ORANGE = [254,100,0]
 COLOUR_WHITE = [254,254,254]
-ACTIVE_COLOUR = COLOUR_BLUE
+ACTIVE_COLOUR = COLOUR_ORANGE
 pixels=[COLOUR_OFF for i in range(60)]
 
 def setColour(colour1, colour2, colour3, colour4, colour5, colour6):
-	pixels[0:9] = [colour1,colour1,colour1,colour1,colour1,colour1,colour1,colour1,colour1,colour1]
-	pixels[10:19] = [colour2,colour2,colour2,colour2,colour2,colour2,colour2,colour2,colour2,colour2]
-	pixels[20:29] = [colour3,colour3,colour3,colour3,colour3,colour3,colour3,colour3,colour3,colour3]
-	pixels[30:39] = [colour4,colour4,colour4,colour4,colour4,colour4,colour4,colour4,colour4,colour4]
-	pixels[40:49] = [colour5,colour5,colour5,colour5,colour5,colour5,colour5,colour5,colour5,colour5]
-	pixels[50:59] = [colour6,colour6,colour6,colour6,colour6,colour6,colour6,colour6,colour6,colour6]
+	for i in range(0,10):
+		pixels[i]=colour1
+	for i in range(10,20):
+		pixels[i]=colour2
+	for i in range(20,30):
+		pixels[i]=colour3
+	for i in range(30,40):
+		pixels[i]=colour4
+	for i in range(40,50):
+		pixels[i]=colour5
+	for i in range(50,60):
+		pixels[i]=colour6
 
 def firstOn():
 	setColour(ACTIVE_COLOUR, COLOUR_OFF, COLOUR_OFF, COLOUR_OFF, ACTIVE_COLOUR, COLOUR_OFF)
--- knightrider.py
+++ knightrider.py
@@ -9,17 +9,28 @@ import math
 bb = BlinkyTape('/dev/ttyACM0') #least on Mac OS X, this is the port to use!
 
 OFF_COLOUR = [0,0,0]
-RED = [254,0,0]
-DISPLAY_LENGTH = 30
-UPDATE_INTERVAL = 0.02
+DISPLAY_COLOUR = [254,0,0]
+R_INTERVAL = DISPLAY_COLOUR[0]/15
+G_INTERVAL = DISPLAY_COLOUR[1]/15
+B_INTERVAL = DISPLAY_COLOUR[2]/15
+DISPLAY_LENGTH = 60
+UPDATE_INTERVAL = 0.01
 
 pixels=[OFF_COLOUR for i in range(DISPLAY_LENGTH)]
 
 def fadePixel(pixel):
 	if pixel[0] != 0:
-		pixel[0] = pixel[0] - 17
+		pixel[0] = pixel[0] - R_INTERVAL
 		if pixel[0] < 0:
 			pixel[0] = 0
+	if pixel[1] != 0:
+		pixel[1] = pixel[1] - G_INTERVAL
+		if pixel[1] < 0:
+			pixel[1] = 0
+	if pixel[2] != 0:
+		pixel[2] = pixel[2] - B_INTERVAL
+		if pixel[2] < 0:
+			pixel[2] = 0
 
 
 def fadePixels():
@@ -29,12 +40,12 @@ def fadePixels():
 def knightrider():
 	for i in range(DISPLAY_LENGTH):
 		fadePixels()
-		pixels[i] = list(RED)
+		pixels[i] = list(DISPLAY_COLOUR)
 		bb.send_list(pixels)
 		sleep(UPDATE_INTERVAL)
 	for i in reversed(range(DISPLAY_LENGTH)):
 		fadePixels()
-		pixels[i] = list(RED)
+		pixels[i] = list(DISPLAY_COLOUR)
 		bb.send_list(pixels)
 		sleep(UPDATE_INTERVAL)
 
